beafac407a4a0eec6ef96cdba77b1142d028aaa6
Peter Karlsson <peter@softwolves.pp.se>
1999-07-15 23:59:53 +0000

    Initial offering of native *.MSG reading routines for Turquoise.

---
+++ sdmread.cpp
@@ -0,0 +1,177 @@
+// Copyright (c) 1999 Peter Karlsson
+//
+// $Id$
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#include <iostream.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#ifdef UNIX
+# include <sys/types.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <unistd.h>
+#endif
+
+#include "sdmread.h"
+
+SdmRead::SdmRead(const char *path, bool hasarrivetime) : isopus(hasarrivetime)
+{
+    areapath = strdup(path);
+}
+
+SdmRead::~SdmRead()
+{
+    if (areapath) delete areapath;
+}
+
+bool SdmRead::Transfer(time_t starttime, StatEngine &destination)
+{
+    // Check that we got the path correctly in initialization
+    if (!areapath)
+    {
+        cerr << "Internal error: Area path was not allocated properly" << endl;
+        return false;
+    }
+
+    // Open the message directory
+#ifdef UNIX
+    DIR *sdmdir = opendir(areapath);
+    if (!sdmdir)
+    {
+        cerr << "Unable to open *.MSG directory" << endl;
+        return false;
+    }
+
+#else
+# error No non-Unix code yet!
+#endif
+
+    string dirname = string(areapath);
+    if (dirname[dirname.length() - 1] != '/')
+    {
+        dirname += '/';
+    }
+
+    sdmhead_s sdmhead;
+    FILE *msg = NULL;
+    unsigned long msglen;
+    char *msgbuf = NULL, *ctrlbuf = NULL;
+    time_t written, arrived;
+    UINT32 msgn = 0;
+
+#ifdef UNIX
+# define FILENAME sdmdirent_p->d_name
+# define FILESIZE sdmstat.st_size
+    struct dirent *sdmdirent_p;
+    struct stat   sdmstat;
+
+    while (NULL != (sdmdirent_p = readdir(sdmdir)))
+#endif
+    {
+        // Check that we really have a *.msg file
+        // Unix readdir gives us all files, and DOS findfirst is buggy
+        string thisfile = dirname + FILENAME;
+        if (fcompare(thisfile.substr(thisfile.length() - 3, 3), "msg"))
+            goto out;
+
+        msg = fopen(thisfile.c_str(), "rb");
+        if (!msg)
+        {
+            cerr << "Unable to open " << thisfile << endl;
+            goto out;
+        }
+
+#ifdef UNIX
+        stat(thisfile.c_str(), &sdmstat);
+#endif
+
+        // Read the message header
+        if (1 != fread(&sdmhead, sizeof (sdmhead_s), 1, msg))
+        {
+            cerr << "Broken MSG file " << thisfile << endl;
+            goto out;
+        }
+
+        msglen = FILESIZE - sizeof (sdmhead_s);
+
+        msgbuf = new char[msglen];
+        if (!msgbuf)
+        {
+            cerr << "Unable to allocate memory for message body ("
+                 << thisfile.c_str() << ')' << endl;
+            goto out2;
+        }
+
+        ctrlbuf = new char[msglen];
+        if (!msgbuf)
+        {
+            cerr << "Unable to allocate memory for control data ("
+                 << thisfile.c_str() << ')' << endl;
+            goto out;
+        }
+
+        fread(msgbuf, msglen, 1, msg);
+        
+        // Separate kludges from text
+        fixupctrlbuffer(msgbuf, ctrlbuf);
+cerr<<"Kludges"<<endl;
+cerr<<"========= "<<strlen(ctrlbuf)<<endl;
+for(int i=0;i<strlen(ctrlbuf);i++)if(1==ctrlbuf[i])cerr<<endl;else
+cerr<<ctrlbuf[i];cerr<<endl;
+cerr<<"========="<<endl;
+cerr<<"Body "<<strlen(msgbuf)<<endl;
+cerr<<"========="<<endl;
+for(int i=0;i<strlen(msgbuf);i++)if(13==msgbuf[i])cerr<<endl;else
+cerr<<msgbuf[i];cerr<<endl;
+cerr<<"========="<<endl;
+
+        written = asciiToTimeT((char *) sdmhead.datetime);
+        if (isopus)
+        {
+            arrived = stampToTimeT(&sdmhead.opus.arrived);
+        }
+        else
+        {
+            arrived = written;
+        }
+
+        // Add to statistics
+        if (written >= starttime)
+        {
+            destination.AddData(string((char *) sdmhead.fromusername),
+                                string((char *) sdmhead.tousername),
+                                string((char *) sdmhead.subject),
+                                string(ctrlbuf), string(msgbuf),
+                                written, arrived);
+        }
+
+        // Clean up our mess
+out:;        
+        delete ctrlbuf;
+out2:;
+        delete msgbuf;
+
+        cout << ++ msgn << " done\r";
+        if (msg) fclose(msg);
+    }
+
+#ifdef UNIX
+    closedir(sdmdir);
+#endif
+
+    return true;
+}
---
+++ sdmread.h
@@ -0,0 +1,80 @@
+// Copyright (c) 1999 Peter Karlsson
+//
+// $Id$
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#ifndef __SDMREAD_H
+#define __SDMREAD_H
+
+#include <string>
+
+#include "arearead.h"
+#include "statengine.h"
+#include "datatypes.h"
+#include "utility.h"
+
+class SdmRead : public AreaRead
+{
+public:
+    // Constructor and destructor
+    SdmRead(const char *path, bool hasarrivetime);
+    virtual ~SdmRead();
+
+    // Transfer function
+    virtual bool Transfer(time_t starttime, StatEngine &destination);
+
+protected:
+    char    *areapath;
+    bool    isopus;
+
+    struct sdmhead_s
+    {
+        UINT8   fromusername[36];
+        UINT8   tousername[36];
+        UINT8   subject[72];
+        UINT8   datetime[20];       // "Dd Mmm Yy  HH:MM:SS"
+                                    // "Www Dd Mmm Yy HH:MM"
+        UINT16  timesread;
+        UINT16  destnode;
+        UINT16  orignode;
+        UINT16  cost;
+        UINT16  orignet;
+        UINT16  destnet;
+        union
+        {
+            struct // FTSC style
+            {
+                UINT16  destzone;
+                UINT16  origzone;
+                UINT16  destpoint;
+                UINT16  origpoint;
+            } ftsc;
+            struct // Opus style
+            {
+                stamp_s written;
+                stamp_s arrived;
+            } opus;
+        };
+        UINT16  replyto;
+        UINT16  attribute;
+        UINT16  nextreply;
+    };
+};
+
+#if defined(__GNUC__) || defined(__EMX__)
+# pragma pack()
+#endif
+
+#endif
