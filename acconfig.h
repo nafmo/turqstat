f150455da5dddbe748c4e5e642d04b7b0e458a85
Peter Karlsson <peter@softwolves.pp.se>
2000-03-19 13:38:33 +0000

    Added autoconf stuff.

---
+++ acconfig.h
@@ -0,0 +1,18 @@
+/* Define if we are using EMX's implementation of findfirst/findnext */
+#undef HAS_EMX_FINDFIRST
+
+/* Define if getopt is found in unistd.h */
+#undef HAS_GETOPT_IN_UNISTD
+
+/* Define if getopt is found in getopt.h */
+#undef HAS_GETOPT_IN_GETOPT
+
+/* Define if struct tm has gmt_off */
+#undef HAS_TM_GMTOFF
+
+/* Define if time.h defines daylight variable */
+#undef HAVE_DAYLIGHT
+
+/* Define if time.h defines timezone variable */
+#undef HAVE_TIMEZONE
+
