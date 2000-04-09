aa4a6f416dc6c04a30280f680cec8fc87f879310
Peter Karlsson <peter@softwolves.pp.se>
2000-04-09 12:54:58 +0000

    Uses configure checks to find correct location of opendir/readdir stuff.

--- newsspoolread.cpp
+++ newsspoolread.cpp
@@ -26,8 +26,22 @@
 #elif defined(HAVE_MINGW32_DIR_H)
 # include <mingw32/dir.h>
 #else
+# ifdef HAVE_DIRENT_H
 # include <dirent.h>
-# include <unistd.h>
+#  define NAMLEN(dirent) strlen((dirent)->d_name)
+# else
+#  define dirent direct
+#  define NAMLEN(dirent) (dirent)->d_namlen
+#  ifdef HAVE_SYS_NDIR_H
+#   include <sys/ndir.h>
+#  endif
+#  ifdef HAVE_SYS_DIR_H
+#   include <sys/dir.h>
+#  endif
+#  ifdef HAVE_NDIR_H
+#   include <ndir.h>
+#  endif
+# endif
 #endif
 #ifndef HAVE_MINGW32_DIR_H
 # include <sys/types.h>
