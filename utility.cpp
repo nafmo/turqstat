afbd9bcbaee0312f466cc50d07719e65ec7a169e
Peter Karlsson <peter@softwolves.pp.se>
2000-04-14 20:19:06 +0000

    Timezone fixes (backported from 1.4 tree).

--- utility.cpp
+++ utility.cpp
@@ -55,7 +55,7 @@ time_t stampToTimeT(struct stamp_s *st)
     tms.tm_mday = st->date.da;
     tms.tm_mon = st->date.mo - 1;
     tms.tm_year = st->date.yr + 80;
-    tms.tm_isdst = -1;
+    tms.tm_isdst = 0;
 #if defined(__GNUC__) && !defined(__EMX__) && !defined(__CYGWIN__)
     tms.tm_gmtoff = 0;
 #endif
@@ -98,6 +98,7 @@ time_t asciiToTimeT(const char *datetime)
     //        better use some sliding-window technique
     if (tms.tm_year < 80) tms.tm_year += 100;
 
+    tms.tm_isdst = 0;
 #if defined(__GNUC__) && !defined(__EMX__) && !defined(__CYGWIN__)
     tms.tm_gmtoff = 0;
 #endif
@@ -132,6 +133,7 @@ time_t rfcToTimeT(string datetime)
     if (!c_p) return 0;
     tms.tm_mon = ((int) (c_p - months)) / 3;
 
+    tms.tm_isdst = 0;
 #if defined(__GNUC__) && !defined(__EMX__) && !defined(__CYGWIN__)
     tms.tm_gmtoff = 0;
 #endif
