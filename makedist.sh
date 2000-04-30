340771232f8e44d1c7b50544ae0e08f4a222b4d6
Peter Karlsson <peter@softwolves.pp.se>
2000-04-30 23:43:10 +0000

    Documentation changes.

--- makedist.sh
+++ makedist.sh
@@ -14,11 +14,15 @@ echo '* Makedist: Turquoise SuperStat version '$ver
 mkdir -p turqstat-$ver/debian
 autoconf
 autoheader
-cp -p *.1 *.def *.cpp *.h Makefile.in config.h.in configure configure.in ChangeLog COPYING turqstat-$ver/
-iso2dos -i turqstat.doc turqstat-$ver/turqstat.doc
-touch -r   turqstat.doc turqstat-$ver/turqstat.doc
-iso2dos -i turqstat.dok turqstat-$ver/turqstat.dok
-touch -r   turqstat.dok turqstat-$ver/turqstat.dok
+cp -p *.def *.cpp *.h Makefile.in config.h.in configure configure.in ChangeLog COPYING turqstat-$ver/
+iso2dos -i turqstat.doc  turqstat-$ver/turqstat.doc
+touch -r   turqstat.doc  turqstat-$ver/turqstat.doc
+iso2dos -i turqstat.dok  turqstat-$ver/turqstat.dok
+touch -r   turqstat.dok  turqstat-$ver/turqstat.dok
+iso2dos -i turqstat.1    turqstat-$ver/turqstat.1
+touch -r   turqstat.1    turqstat-$ver/turqstat.1
+iso2dos -i turqstat-sv.1 turqstat-$ver/turqstat-sv.1
+touch -r   turqstat-sv.1 turqstat-$ver/turqstat-sv.1
 cp -p debian/* turqstat-$ver/debian
 fakeroot tar cvfz turqstat-$ver.tar.gz turqstat-$ver/*
 cd turqstat-$ver
