3cf684ae7c0134791c6a481687747cc36a625d47
Peter Karlsson <peter@softwolves.pp.se>
2000-10-17 19:04:17 +0000

    Added nodeb parameter to stop building Debian archives (uncommitted since     July 16th)

--- makedist.sh
+++ makedist.sh
@@ -9,13 +9,17 @@ else
   ver=$1
 fi
 
+if [ "$2" == "nodeb" ]; then
+  echo "Bygger inget Debianarkiv"
+fi
+
 echo '* Makedist: Turquoise SuperStat version '$ver
 
 rm -rf turqstat-$ver/
 mkdir -p turqstat-$ver/debian
 autoconf
 autoheader
-cp -p *.def *.cpp *.h Makefile.in config.h.in configure configure.in ChangeLog COPYING turqstat-$ver/
+cp -p *.spec *.def *.cpp *.h Makefile.in config.h.in configure configure.in ChangeLog COPYING turqstat-$ver/
 rm turqstat-$ver/config.h
 iso2dos -i turqstat.doc  turqstat-$ver/turqstat.doc
 touch -r   turqstat.doc  turqstat-$ver/turqstat.doc
@@ -26,8 +30,11 @@ touch -r   turqstat.1    turqstat-$ver/turqstat.1
 iso2dos -i turqstat-sv.1 turqstat-$ver/turqstat-sv.1
 touch -r   turqstat-sv.1 turqstat-$ver/turqstat-sv.1
 cp -p debian/* turqstat-$ver/debian
+
 fakeroot tar cvfz turqstat-$ver.tar.gz turqstat-$ver/*
-cd turqstat-$ver
-fakeroot debian/rules binary
-cd ..
+if [ "$2" != "nodeb" ]; then
+  cd turqstat-$ver
+  fakeroot debian/rules binary
+  cd ..
+fi
 rm -rf turqstat-$ver/
