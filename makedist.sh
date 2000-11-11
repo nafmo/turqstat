e805a66b22a5bd984a9cf4beb9b2cd9969f2d702
Peter Karlsson <peter@softwolves.pp.se>
2000-11-11 21:14:36 +0000

    Updated for Qt. Possible not to create DEB.

--- makedist.sh
+++ makedist.sh
@@ -9,6 +9,10 @@ else
   ver=$1
 fi
 
+if [ "$2" == "nodeb" ]; then
+  echo "Bygger inget Debianarkiv"
+fi
+
 echo '* Makedist: Turquoise SuperStat version '$ver
 
 rm -rf turqstat-$ver/
@@ -17,6 +21,7 @@ autoconf
 autoheader
 cp -p *.def *.cpp *.h Makefile.in config.h.in configure configure.in ChangeLog COPYING turqstat-$ver/
 rm turqstat-$ver/config.h
+rm turqstat-$ver/moc*.cpp
 iso2dos -i turqstat.doc  turqstat-$ver/turqstat.doc
 touch -r   turqstat.doc  turqstat-$ver/turqstat.doc
 iso2dos -i turqstat.dok  turqstat-$ver/turqstat.dok
@@ -26,8 +31,11 @@ touch -r   turqstat.1    turqstat-$ver/turqstat.1
 iso2dos -i turqstat-sv.1 turqstat-$ver/turqstat-sv.1
 touch -r   turqstat-sv.1 turqstat-$ver/turqstat-sv.1
 cp -p debian/* turqstat-$ver/debian
+
 fakeroot tar cvfz turqstat-$ver.tar.gz turqstat-$ver/*
-cd turqstat-$ver
-fakeroot debian/rules binary
-cd ..
+if [ "$2" != "nodeb" ]; then
+  cd turqstat-$ver
+  fakeroot debian/rules binary
+  cd ..
+fi
 rm -rf turqstat-$ver/
