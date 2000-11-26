8d73ab22ce5d5fd3b5c77697331808fb020b325b
Peter Karlsson <peter@softwolves.pp.se>
2000-11-26 13:21:56 +0000

    Updates for 2.0

--- makedist.sh
+++ makedist.sh
@@ -16,21 +16,31 @@ fi
 echo '* Makedist: Turquoise SuperStat version '$ver
 
 rm -rf turqstat-$ver/
-mkdir -p turqstat-$ver/debian
+mkdir -p turqstat-$ver/debian turqstat-$ver/po
 autoconf
 autoheader
 cp -p *.spec *.def *.cpp *.h Makefile.in config.h.in configure configure.in ChangeLog COPYING turqstat-$ver/
 rm turqstat-$ver/config.h
 rm turqstat-$ver/moc*.cpp
-iso2dos -i turqstat.doc  turqstat-$ver/turqstat.doc
-touch -r   turqstat.doc  turqstat-$ver/turqstat.doc
-iso2dos -i turqstat.dok  turqstat-$ver/turqstat.dok
-touch -r   turqstat.dok  turqstat-$ver/turqstat.dok
-iso2dos -i turqstat.1    turqstat-$ver/turqstat.1
-touch -r   turqstat.1    turqstat-$ver/turqstat.1
-iso2dos -i turqstat-sv.1 turqstat-$ver/turqstat-sv.1
-touch -r   turqstat-sv.1 turqstat-$ver/turqstat-sv.1
+
+#recode 437/..l1
+iso2dos -i turqstat.doc   turqstat-$ver/turqstat.doc
+touch -r   turqstat.doc   turqstat-$ver/turqstat.doc
+iso2dos -i turqstat.dok   turqstat-$ver/turqstat.dok
+touch -r   turqstat.dok   turqstat-$ver/turqstat.dok
+
+iso2dos -i turqstat.1     turqstat-$ver/turqstat.1
+touch -r   turqstat.1     turqstat-$ver/turqstat.1
+iso2dos -i turqstat-sv.1  turqstat-$ver/turqstat-sv.1
+touch -r   turqstat-sv.1  turqstat-$ver/turqstat-sv.1
+
+iso2dos -i xturqstat.1    turqstat-$ver/xturqstat.1
+touch -r   xturqstat.1    turqstat-$ver/xturqstat.1
+iso2dos -i xturqstat-sv.1 turqstat-$ver/xturqstat-sv.1
+touch -r   xturqstat-sv.1 turqstat-$ver/xturqstat-sv.1
+
 cp -p debian/* turqstat-$ver/debian
+cp -p po/*.po po/update-po.sh po/Makefile.in turqstat-$ver/po/
 
 fakeroot tar cvfz turqstat-$ver.tar.gz turqstat-$ver/*
 if [ "$2" != "nodeb" ]; then
