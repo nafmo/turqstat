6efa8c491b05b8039eceb1adb07d1f57d389c724
Peter Karlsson <peter@softwolves.pp.se>
2001-05-16 20:11:00 +0000

    Changes to accomodate documentation files in ISO-8859-1

--- makedist.sh
+++ makedist.sh
@@ -23,22 +23,7 @@ cp -p *.spec *.def *.cpp *.h Makefile.in config.h.in doxygen.conf.in configure c
 rm turqstat-$ver/config.h
 rm turqstat-$ver/moc*.cpp
 
-#recode 437/..l1
-iso2dos -i turqstat.doc   turqstat-$ver/turqstat.doc
-touch -r   turqstat.doc   turqstat-$ver/turqstat.doc
-iso2dos -i turqstat.dok   turqstat-$ver/turqstat.dok
-touch -r   turqstat.dok   turqstat-$ver/turqstat.dok
-
-iso2dos -i turqstat.1     turqstat-$ver/turqstat.1
-touch -r   turqstat.1     turqstat-$ver/turqstat.1
-iso2dos -i turqstat-sv.1  turqstat-$ver/turqstat-sv.1
-touch -r   turqstat-sv.1  turqstat-$ver/turqstat-sv.1
-
-iso2dos -i xturqstat.1    turqstat-$ver/xturqstat.1
-touch -r   xturqstat.1    turqstat-$ver/xturqstat.1
-iso2dos -i xturqstat-sv.1 turqstat-$ver/xturqstat-sv.1
-touch -r   xturqstat-sv.1 turqstat-$ver/xturqstat-sv.1
-
+cp -p turqstat.do{c,k} {,x}turqstat{-sv,}.1 turqstat-$ver/
 cp -p debian/* turqstat-$ver/debian
 cp -p po/*.po po/update-po.sh po/Makefile.in turqstat-$ver/po/
 cp -p mappings/*.txt mappings/Makefile.in mappings/*.{cpp,h,pl} turqstat-$ver/mappings/
