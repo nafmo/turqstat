c7a10e362c32114dd4d921a2d8746fe3eef8350f
Peter Karlsson <peter@softwolves.pp.se>
2007-08-27 06:32:18 +0100

    Updated copyright headers.

--- utility.h
+++ utility.h
@@ -1,4 +1,4 @@
-// Copyright (c) 1998-2005 Peter Karlsson
+// Copyright (c) 1998-2007 Peter Karlsson
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
