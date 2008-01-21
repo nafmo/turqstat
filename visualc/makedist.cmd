3639485b13497c102ddfc5c79f2bb39d931431a9
Peter Karlsson <peter@softwolves.pp.se>
2008-01-21 21:23:14 +0100

    Need the README file as well

--- visualc/makedist.cmd
+++ visualc/makedist.cmd
@@ -6,7 +6,7 @@ if "%1" == "" goto help
 if not exist visualc\Release\turqstat.exe goto build
 
 call git archive --format=zip -9 --prefix=src/ HEAD > visualc\turqstat-%1.zip
-zip -9j visualc/turqstat-%1.zip visualc/Release/turqstat.exe turqstat.doc turqstat.dok
+zip -9j visualc/turqstat-%1.zip visualc/Release/turqstat.exe turqstat.doc turqstat.dok README.Win32
 
 goto end
 
