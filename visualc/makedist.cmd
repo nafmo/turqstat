42682de28bacae8c28650b14b0c0cb5ef2211200
Peter Karlsson <peter@softwolves.pp.se>
2008-01-21 21:17:06 +0100

    Added simple release script for Win32.

---
+++ visualc/makedist.cmd
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+cd ..
+
+if "%1" == "" goto help
+if not exist visualc\Release\turqstat.exe goto build
+
+call git archive --format=zip -9 --prefix=src/ HEAD > visualc\turqstat-%1.zip
+zip -9j visualc/turqstat-%1.zip visualc/Release/turqstat.exe turqstat.doc turqstat.dok
+
+goto end
+
+:help
+echo Usage: %0 version
+goto end
+
+:build
+echo Must build with Visual C++ first
+goto end
+
+:end
+endlocal
