65a6d7f5b1778cb117e5e018c091d7b65e1b9c48
Peter Karlsson <peter@softwolves.pp.se>
2008-01-21 21:20:05 +0100

    Simplified release tarball release script.

--- makedist.sh
+++ makedist.sh
@@ -8,29 +8,4 @@ else
   ver=$1
 fi
 
-if [ "$2" == "nodeb" ]; then
-  echo "Bygger inget Debianarkiv"
-fi
-
-echo '* Makedist: Turquoise SuperStat version '$ver
-
-rm -rf turqstat-$ver/
-mkdir -p turqstat-$ver/debian turqstat-$ver/po turqstat-$ver/mappings
-autoconf
-autoheader
-cp -p *.spec *.def *.cpp *.h Makefile.in config.h.in doxygen.conf.in configure configure.in ChangeLog COPYING turqstat-$ver/
-rm turqstat-$ver/config.h
-rm turqstat-$ver/moc*.cpp
-
-cp -p turqstat.do{c,k} {,x}turqstat{-sv,}.1 turqstat-$ver/
-cp -p debian/* turqstat-$ver/debian
-cp -p po/*.po po/update-po.sh po/Makefile.in turqstat-$ver/po/
-cp -p mappings/*.txt mappings/Makefile.in mappings/*.{cpp,h,pl} turqstat-$ver/mappings/
-
-fakeroot tar cvfz turqstat_$ver.tar.gz turqstat-$ver/*
-if [ "$2" != "nodeb" ]; then
-  cd turqstat-$ver
-  fakeroot debian/rules binary
-  cd ..
-fi
-rm -rf turqstat-$ver/
+git archive --format=tar --prefix=turqstat-$ver/ HEAD | gzip -9 > turqstat-$ver.tar.gz
